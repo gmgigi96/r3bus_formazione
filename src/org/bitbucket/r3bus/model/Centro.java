f4d051c22dd64a368ee807e50de5006e35f56840
Gianmaria Del Monte <g.macmount@gmail.com>
2018-05-16 15:09:36 +0200

    Definite classi Rebus e Centro

---
+++ src/org/bitbucket/r3bus/model/Centro.java
@@ -0,0 +1,15 @@
+package org.bitbucket.r3bus.model;
+
+import java.util.Collection;
+
+public class Centro {
+	
+	public Attivita getAttivita(int codiceAttivita) {
+		
+	}
+	
+	public Collection<Attivita> getAttivitaDisponibili() {
+		
+	}	
+	
+}
---
+++ src/org/bitbucket/r3bus/model/Rebus.java
@@ -0,0 +1,24 @@
+package org.bitbucket.r3bus.model;
+
+public class Rebus {
+	
+	private Allievo allievoCorrente;
+	private Azienda azienda;
+	
+	public void gestisciAllievo(String codiceFiscale) {
+		
+	}
+	
+	public void terminaGestione() {
+		
+	}
+	
+	public void prenotaAttivita(int codiceAttivita) {
+		
+	}
+	
+	public void annullaPrenotazione(int codiceAttivita) {
+		
+	}	
+	
+}
