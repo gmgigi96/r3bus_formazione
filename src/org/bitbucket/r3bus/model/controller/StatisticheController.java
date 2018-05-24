8f43b938b43eb1f4fd169e845856858076c4284e
Omar Elsayed <ramorimo93@gmail.com>
2018-05-24 15:45:02 +0200

    Modificata implementazione metodo getStatistiche

--- src/org/bitbucket/r3bus/model/controller/StatisticheController.java
+++ src/org/bitbucket/r3bus/model/controller/StatisticheController.java
@@ -1,6 +1,9 @@
 package org.bitbucket.r3bus.model.controller;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 import java.util.TreeSet;
 
@@ -8,31 +11,33 @@ import org.bitbucket.r3bus.model.Attivita;
 import org.bitbucket.r3bus.model.Centro;
 
 public class StatisticheController {
-	
+
 	private LocalDateTime intervalloTemporaleDa;
 	private LocalDateTime intervalloTemporaleA;
-	
+
 	private final Set<Centro> centri;
-	
+
 	public StatisticheController() {
 		centri = new TreeSet<>();
 	}
-	
+
 	public void setIntervalloTemporale(LocalDateTime da, LocalDateTime a) {
 		this.intervalloTemporaleDa = da;
 		this.intervalloTemporaleA = a;
 	}
-	
+
 	public void addCentro(Centro centro) {
 		this.centri.add(centro);
 	}
-	
-	public Set<Attivita> getStatistiche() {
-		Set<Attivita> attivita = new TreeSet<>();
+
+	public Map<Centro,Set<Attivita>> getStatistiche() {
+		Map<Centro, Set<Attivita>> centro2Attivita = new HashMap<>();
 		for (Centro c : centri) {
+			Set<Attivita> attivita = new HashSet<>();
 			attivita.addAll(c.getAttivitaInIntervallo(intervalloTemporaleDa, intervalloTemporaleA));
+			centro2Attivita.put(c, attivita);
 		}
-		return attivita;
+		return centro2Attivita;
 	}
-	
+
 }
