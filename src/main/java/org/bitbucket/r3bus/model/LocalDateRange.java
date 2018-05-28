ba9b4535c6104456a0c137fb4ab584a84ca64bdc
Gianmaria Del Monte <g.macmount@gmail.com>
2018-05-28 17:14:03 +0200

    Introdotta classe LocalDateRange per gestire il range tra due date

---
+++ src/main/java/org/bitbucket/r3bus/model/LocalDateRange.java
@@ -0,0 +1,47 @@
+package org.bitbucket.r3bus.model;
+
+import java.time.LocalDate;
+import java.util.Iterator;
+
+public class LocalDateRange implements Iterable<LocalDate> {
+	
+	private Iterator<LocalDate> iterator;
+	
+	public static Iterable<LocalDate> with(LocalDate inizio, LocalDate fine) {
+		return new LocalDateRange(inizio,fine);
+	}
+	
+	private LocalDateRange(LocalDate inizio, LocalDate fine) {
+		iterator = new LocalDateRangeIterator(inizio, fine);
+	}
+
+	private class LocalDateRangeIterator implements Iterator<LocalDate> {
+		
+		private LocalDate inizio;
+		private LocalDate fine;
+		
+		public LocalDateRangeIterator(LocalDate inizio, LocalDate fine) {
+			this.inizio = inizio;
+			this.fine = fine;
+		}
+		
+		@Override
+		public boolean hasNext() {
+			return inizio.compareTo(fine) <= 0;
+		}
+
+		@Override
+		public LocalDate next() {
+			LocalDate temp = inizio;
+			inizio = inizio.plusDays(1);
+			return temp;
+		}		
+		
+	}	
+	
+	@Override
+	public Iterator<LocalDate> iterator() {
+		return iterator;		
+	}	
+	
+}
