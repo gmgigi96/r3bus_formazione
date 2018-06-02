1565ed81f723a7341828281b25653d592a915259
Matteo Bernardini <mat_teobe@yahoo.it>
2018-06-02 16:20:52 +0200

    (html) templatized the demo template, the thymeleaf way. - Drafted an error page. - Added controller endpoints.

--- src/main/java/org/bitbucket/r3bus/controller/MainController.java
+++ src/main/java/org/bitbucket/r3bus/controller/MainController.java
@@ -1,12 +1,27 @@
 package org.bitbucket.r3bus.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
-	@RequestMapping("/")
+	@GetMapping("/")
 	public String indexPage() {
 		return "index";
+	}
+
+	@GetMapping("/about")
+	public String aboutPage() {
+		return "about";
+	}
+
+	@GetMapping("/contact")
+	public String contactPage() {
+		return "contact";
+	}
+
+	@GetMapping("/portfolio")
+	public String portfoliotPage() {
+		return "portfolio";
 	}
 }
