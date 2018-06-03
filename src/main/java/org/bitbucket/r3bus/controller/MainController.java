81920c133ffadce9fc20e8aecc16aded7ba497b0
Omar Elsayed <ramorimo90@hotmail.com>
2018-06-03 18:11:09 +0200

    (bugfix) spostato index.html in templates

--- src/main/java/org/bitbucket/r3bus/controller/MainController.java
+++ src/main/java/org/bitbucket/r3bus/controller/MainController.java
@@ -1,11 +1,11 @@
 package org.bitbucket.r3bus.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
-	@RequestMapping("/")
+	@GetMapping("/")
 	public String indexPage() {
 		return "index";
 	}
