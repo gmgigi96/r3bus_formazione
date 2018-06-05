c64d11ffe418dfb4c98950796ccbc65ddad38d05
Matteo Bernardini <mat_teobe@yahoo.it>
2018-06-05 17:29:00 +0200

    (ui) new learner page: using spring binding.

--- src/main/java/org/bitbucket/r3bus/controller/MainController.java
+++ src/main/java/org/bitbucket/r3bus/controller/MainController.java
@@ -3,6 +3,8 @@ package org.bitbucket.r3bus.controller;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
+import javax.validation.Valid;
+
 import org.bitbucket.r3bus.model.Allievo;
 import org.bitbucket.r3bus.model.Attivita;
 import org.bitbucket.r3bus.model.Centro;
@@ -11,7 +13,11 @@ import org.bitbucket.r3bus.service.AttivitaService;
 import org.bitbucket.r3bus.service.CentroService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -34,9 +40,20 @@ public class MainController {
 		return "login";
 	}
 
-	@RequestMapping("/allievo/nuovo")
-	public String newLearnerPage() {
+	@GetMapping("/allievo/nuovo")
+	public String newLearnerForm(ModelMap model) {
+		model.addAttribute("learner", new Allievo()); // TOFIX
 		return "new_learner";
+	}
+
+	@PostMapping("/allievo/nuovo")
+	public String saveNewLearner(@Valid @ModelAttribute("learner") Allievo learner, BindingResult bindingResult,
+			ModelMap model) {
+		if (bindingResult.hasErrors())
+			return "new_learner";
+		// add to db / call grasp controller
+		model.clear();
+		return "redirect:/";
 	}
 
 	/* ↓↓ mapping di prova (da eliminare) */
