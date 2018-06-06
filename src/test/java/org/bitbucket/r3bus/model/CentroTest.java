dbd079188550df76efc7b94269f8095da38e6b31
Gianmaria Del Monte <g.macmount@gmail.com>
2018-06-06 19:25:58 +0200

    (test) Eliminati import inutili

--- src/test/java/org/bitbucket/r3bus/model/CentroTest.java
+++ src/test/java/org/bitbucket/r3bus/model/CentroTest.java
@@ -4,13 +4,8 @@ import static org.junit.Assert.assertEquals;
 
 import java.time.LocalDateTime;
 
-import org.bitbucket.r3bus.service.CentroService;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 public class CentroTest {
 	
