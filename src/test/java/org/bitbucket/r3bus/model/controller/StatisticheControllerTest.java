55b14b15aa665a12bb1dd61a253636fa7830e068
Gianmaria Del Monte <g.macmount@gmail.com>
2018-06-07 17:00:03 +0200

    (test) Aggiunti test per getmediaPrenotati di StatisticheController

--- src/test/java/org/bitbucket/r3bus/model/controller/StatisticheControllerTest.java
+++ src/test/java/org/bitbucket/r3bus/model/controller/StatisticheControllerTest.java
@@ -80,67 +80,117 @@ public class StatisticheControllerTest {
 	public void testGetElencoAttivita_centroConNessunaAttivita() {
 		LocalDate inizio = LocalDate.MIN;
 		LocalDate fine = LocalDate.MAX;
-		
+
 		statisticheController1.setCentro(centroConZeroAttivita);
 		statisticheController1.setIntervallo(inizio, fine);
-		
-		assertTrue(statisticheController1.getElencoAttivita().isEmpty());
-	}
-	
-	@Test
-	public void testGetElencoAttivita_centroConUnaAttivita_zeroPartecipanti() {
-		LocalDate inizio = LocalDate.MIN;
-		LocalDate fine = LocalDate.MAX;
-		
-		LocalDateTime now = LocalDateTime.now();
-		
-		Centro centroConUnAttivita_zeroPartecipanti = new Centro();
-		centroConUnAttivita_zeroPartecipanti.addAttivita("attivitaZeroPartecipanti", now, now.plusHours(1));
-		
-		Map<String, Integer> res = statisticheController1.getElencoAttivita();
-		assertEquals(new Integer(0), res.get("attivitaZeroPartecipanti"));
-		assertEquals(1, res.values().size());
-	}
-	
-	@Test
-	public void testGetElencoAttivita_centroConUnaAttivita_unPartecipante() {
-		LocalDate inizio = LocalDate.MIN;
-		LocalDate fine = LocalDate.MAX;
-		
-		LocalDateTime now = LocalDateTime.now();
-		
-		Centro centroConUnAttivita_unPartecipante = new Centro();
-		Attivita attivitaConUnPartecipante = new Attivita("attivitaConUnPartecipante", now, now.plusHours(3));
-		attivitaConUnPartecipante.prenota(new Allievo());
-		
-		centroConUnAttivita_unPartecipante.addAttivita(attivitaConUnPartecipante);
-		
-		statisticheController1.setCentro(centroConUnAttivita_unPartecipante);
-		statisticheController1.setIntervallo(inizio, fine);
-		
-		Map<String, Integer> res = statisticheController1.getElencoAttivita();
-		assertEquals(new Integer(1), res.get("attivitaConUnPartecipante"));
-		assertEquals(1, res.values().size());
-	}
-	
-	@Test
-	public void testGetElencoAttivita_centroConUnaAttivita_zeroPartecipanti_intervalloNonComprendenteAttivita() {
-		LocalDateTime t = LocalDateTime.of(2018, 10, 10, 10, 0);		
-		
-		Centro centroConUnAttivita_zeroPartecipanti = new Centro();
-		Attivita attivitaConZeroPartecipanti = new Attivita("attivitaConZeroPartecipanti", t, t.plusHours(3));
-		
-		centroConUnAttivita_zeroPartecipanti.addAttivita(attivitaConZeroPartecipanti);
-		
-		statisticheController1.setCentro(centroConUnAttivita_zeroPartecipanti);
-		statisticheController1.setIntervallo(t.toLocalDate().minusDays(10), t.toLocalDate().minusDays(1));
-		
+
 		assertTrue(statisticheController1.getElencoAttivita().isEmpty());
 	}
 
 	@Test
-	public void testGetMediaPrenotati() {
-		fail("Not yet implemented");
+	public void testGetElencoAttivita_centroConUnaAttivita_zeroPartecipanti() {
+		LocalDate inizio = LocalDate.MIN;
+		LocalDate fine = LocalDate.MAX;
+
+		LocalDateTime now = LocalDateTime.now();
+
+		Centro centroConUnAttivita_zeroPartecipanti = new Centro();
+		centroConUnAttivita_zeroPartecipanti.addAttivita("attivitaZeroPartecipanti", now, now.plusHours(1));
+
+		Map<String, Integer> res = statisticheController1.getElencoAttivita();
+		assertEquals(new Integer(0), res.get("attivitaZeroPartecipanti"));
+		assertEquals(1, res.values().size());
+	}
+
+	@Test
+	public void testGetElencoAttivita_centroConUnaAttivita_unPartecipante() {
+		LocalDate inizio = LocalDate.MIN;
+		LocalDate fine = LocalDate.MAX;
+
+		LocalDateTime now = LocalDateTime.now();
+
+		Centro centroConUnAttivita_unPartecipante = new Centro();
+		Attivita attivitaConUnPartecipante = new Attivita("attivitaConUnPartecipante", now, now.plusHours(3));
+		attivitaConUnPartecipante.prenota(new Allievo());
+
+		centroConUnAttivita_unPartecipante.addAttivita(attivitaConUnPartecipante);
+
+		statisticheController1.setCentro(centroConUnAttivita_unPartecipante);
+		statisticheController1.setIntervallo(inizio, fine);
+
+		Map<String, Integer> res = statisticheController1.getElencoAttivita();
+		assertEquals(new Integer(1), res.get("attivitaConUnPartecipante"));
+		assertEquals(1, res.values().size());
+	}
+
+	@Test
+	public void testGetElencoAttivita_centroConUnaAttivita_zeroPartecipanti_intervalloNonComprendenteAttivita() {
+		LocalDateTime t = LocalDateTime.of(2018, 10, 10, 10, 0);
+
+		Centro centroConUnAttivita_zeroPartecipanti = new Centro();
+		Attivita attivitaConZeroPartecipanti = new Attivita("attivitaConZeroPartecipanti", t, t.plusHours(3));
+
+		centroConUnAttivita_zeroPartecipanti.addAttivita(attivitaConZeroPartecipanti);
+
+		statisticheController1.setCentro(centroConUnAttivita_zeroPartecipanti);
+		statisticheController1.setIntervallo(t.toLocalDate().minusDays(10), t.toLocalDate().minusDays(1));
+
+		assertTrue(statisticheController1.getElencoAttivita().isEmpty());
+	}
+
+	@Test
+	public void testGetMediaPrenotati_centroConZeroAttivita() {
+		LocalDate inizio = LocalDate.of(2018, 10, 10);
+		LocalDate fine = inizio.plusDays(1);
+
+		statisticheController1.setCentro(centroConZeroAttivita);
+		statisticheController1.setIntervallo(inizio, fine);
+
+		List<Float> res = statisticheController1.getMediaPrenotati();
+
+		assertEquals(2, res.size());
+		assertEquals(new Float(0), res.get(0));
+		assertEquals(new Float(0), res.get(1));
+	}
+
+	@Test
+	public void testGetMediaPrenotati_centroConUnaAttivita_zeroPartecipanti() {
+		LocalDateTime inizio = LocalDateTime.of(2018, 10, 10, 10, 0);
+		LocalDateTime fine = inizio.plusHours(3);
+
+		Centro centroConUnAttivita_zeroPartecipanti = new Centro();
+		centroConUnAttivita_zeroPartecipanti.addAttivita("attivitaConZeroPartecipanti", inizio, fine);
+
+		statisticheController1.setCentro(centroConUnAttivita_zeroPartecipanti);
+		statisticheController1.setIntervallo(inizio.toLocalDate().minusDays(1), fine.toLocalDate().plusDays(1));
+
+		List<Float> res = statisticheController1.getMediaPrenotati();
+
+		assertEquals(3, res.size());
+		assertEquals(new Float(0), res.get(0));
+		assertEquals(new Float(0), res.get(1));
+		assertEquals(new Float(0), res.get(2));
+	}
+
+	@Test
+	public void testGetMediaPrenotati_centroConUnaAttivita_unPartecipante() {
+		LocalDateTime inizio = LocalDateTime.of(2018, 10, 10, 10, 0);
+		LocalDateTime fine = inizio.plusHours(3);
+
+		Centro centroConUnAttivita_unPartecipante = new Centro();
+		Attivita attivitaConUnPartecipante = new Attivita("attivitaConUnPartecipante", inizio, fine);
+		attivitaConUnPartecipante.prenota(new Allievo());
+		centroConUnAttivita_unPartecipante.addAttivita(attivitaConUnPartecipante);
+
+		statisticheController1.setCentro(centroConUnAttivita_unPartecipante);
+		statisticheController1.setIntervallo(inizio.toLocalDate().minusDays(1), fine.toLocalDate().plusDays(1));
+
+		List<Float> res = statisticheController1.getMediaPrenotati();
+
+		assertEquals(3, res.size());
+		assertEquals(new Float(0), res.get(0));
+		assertEquals(new Float(1), res.get(1));
+		assertEquals(new Float(0), res.get(2));
 	}
 
 }
