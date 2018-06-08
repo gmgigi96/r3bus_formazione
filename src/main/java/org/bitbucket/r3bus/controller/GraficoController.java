9d5aeb7ef07af4ff3aa7c4f6de9a4ed700613723
Omar Elsayed <ramorimo90@hotmail.com>
2018-06-08 12:38:29 +0200

    (mvc) completato controller per i grafici

--- src/main/java/org/bitbucket/r3bus/controller/GraficoController.java
+++ src/main/java/org/bitbucket/r3bus/controller/GraficoController.java
@@ -1,41 +1,51 @@
 package org.bitbucket.r3bus.controller;
 
-import java.awt.Graphics2D;
-import java.awt.geom.Rectangle2D;
-import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.time.LocalDate;
-import java.util.HashMap;
-import java.util.Map;
 
 import javax.servlet.http.HttpServletResponse;
 
-import org.bitbucket.r3bus.model.ChartGenerator;
+import org.bitbucket.r3bus.model.Centro;
 import org.bitbucket.r3bus.model.controller.Rebus;
 import org.jfree.chart.ChartUtils;
 import org.jfree.chart.JFreeChart;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class GraficoController {
 	
+	@Autowired
 	Rebus rebus;
 	
-	@RequestMapping("/grafico")
-	public void plot(HttpServletResponse response ) {
-		ChartGenerator chartGenerator = new ChartGenerator();
-		Map<LocalDate, Long> mappa = new HashMap<>();
-		mappa.put(LocalDate.now(), (long) 3);
-		mappa.put(LocalDate.now().plusDays(1), (long) 2);
-		mappa.put(LocalDate.now().plusDays(5), (long) 5);
-
-		JFreeChart chart = chartGenerator.creaGraficoAttivitaGiornaliere(mappa);
-		OutputStream out;
+	@RequestMapping("/direttore/{centro}/{mese}/statistiche/attivita_giornaliere.png")
+	public void graficoAttivitaGiornaliere(@PathVariable("centro") int codiceCentro, @PathVariable("mese") LocalDate mese, HttpServletResponse response ) {
+		LocalDate inizio = mese.withDayOfMonth(1);
+		LocalDate fine = mese.withDayOfMonth(mese.lengthOfMonth());
+		rebus.setCentroGestito(codiceCentro);
 		
+		JFreeChart chart = rebus.creaGraficoAttivitaGiornaliere(inizio, fine);
+
+		creaImmagineGrafico(response, chart);
+	}
+
+	@RequestMapping("/direttore/{centro}/{mese}/statistiche/prenotazioni_giornaliere.png")
+	public void graficoPrenotazioniGiornaliere(@PathVariable("centro") int codiceCentro, @PathVariable("mese") LocalDate mese, HttpServletResponse response ) {
+		LocalDate inizio = mese.withDayOfMonth(1);
+		LocalDate fine = mese.withDayOfMonth(mese.lengthOfMonth());
+		rebus.setCentroGestito(codiceCentro);
+		
+		JFreeChart chart = rebus.creaGraficoPrenotazioniGiornaliere(inizio, fine);
+
+		creaImmagineGrafico(response, chart);
+	}
+
+	private void creaImmagineGrafico(HttpServletResponse response, JFreeChart chart) {
 		try {
-			out = response.getOutputStream();
+			OutputStream out = response.getOutputStream();
 			ChartUtils.writeChartAsPNG(out, chart, 500, 500);
 			response.flushBuffer();
 		} catch (IOException e) {
@@ -43,18 +53,6 @@ public class GraficoController {
 			e.printStackTrace();
 		}
 		
-		 response.setContentType("image/grafico.png");
+		response.setContentType("image/png");
 	}
-
-	public BufferedImage extractImage(JFreeChart chart, int width, int height) {
-		BufferedImage img = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
-
-		Graphics2D g2 = img.createGraphics();
-		chart.draw(g2, new Rectangle2D.Double(0, 0, width, height));
-
-		g2.dispose();
-		return img;
-
-	}
-
 }
