576b53a252ac97245deb67ea316c2319d03bbbdb
Omar Elsayed <ramorimo90@hotmail.com>
2018-06-11 17:13:19 +0200

    (model) cambiate politiche di fetch di allievo e attivita, per permettere il cariacamento in memoria delle relazioni

--- src/main/java/org/bitbucket/r3bus/model/Attivita.java
+++ src/main/java/org/bitbucket/r3bus/model/Attivita.java
@@ -7,6 +7,7 @@ import java.util.List;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -28,7 +29,7 @@ public class Attivita {
 	@Column(nullable = false)
 	private LocalDateTime orarioFine;
 
-	@ManyToMany(cascade = CascadeType.PERSIST)
+	@ManyToMany(fetch=FetchType.EAGER, cascade = CascadeType.PERSIST)
 	private final List<Allievo> allieviPrenotati;
 
 	public Attivita() {
