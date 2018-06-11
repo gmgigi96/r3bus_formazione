1ee84c79a6b1e6dfe9aba4b8b19b34f84bd80a58
Matteo Bernardini <mat_teobe@yahoo.it>
2018-06-11 15:48:15 +0200

    (lint) typo in file name, removed extra \t.

--- init/populate.sql
+++ init/populate.sql
@@ -12,12 +12,12 @@ insert into attivita(id, nome, orario_inizio, orario_fine, centro_id)
 insert into attivita(id, nome, orario_inizio, orario_fine, centro_id) 
 		values('8','Soccorso in mare','2018-08-12 9:00:00','2018-08-12 11:00:00','1');
 
-				
+
 insert into allievo(id, codice_fiscale, nome, cognome, email, luogo_nascita, data_nascita, telefono) 
 	values('9', 'MRAJMP81MARIO', 'Mario','Jumpman','super.mario@nintendo.com','Mario Land','1981-08-03','0101010101');
 insert into allievo(id, codice_fiscale, nome, cognome, email, luogo_nascita, data_nascita, telefono) 
 	values('10','LGUJMP81MARIO', 'Luigi','Jumpman','super.luigi@nintendo.com','Mario Land','1981-08-03','1101010101');
 
-	
+
 insert into attivita_allievi_prenotati(allievi_prenotati_id, attivita_prenotate_id) values('9', '4');
 insert into attivita_allievi_prenotati(allievi_prenotati_id, attivita_prenotate_id) values('9', '8');
