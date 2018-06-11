0875c3c0508ef03b1aea7c1011ae9653932be8a3
Gianmaria Del Monte <g.macmount@gmail.com>
2018-06-11 12:00:29 +0200

    (security) Ora le password devono essere criptate nel db

--- src/main/java/org/bitbucket/r3bus/SecurityConfig.java
+++ src/main/java/org/bitbucket/r3bus/SecurityConfig.java
@@ -4,13 +4,14 @@ import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -24,11 +25,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Autowired
 	private DataSource dataSource;
 
-//	@Bean
-//	public BCryptPasswordEncoder bCryptPasswordEncoder() {
-//		return new BCryptPasswordEncoder();
-//	}
-
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.jdbcAuthentication().dataSource(dataSource)
@@ -36,30 +32,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.usersByUsernameQuery(userQuery)
 			.authoritiesByUsernameQuery(authQuery);
 	}
-
-	// @Override
-	// public void configure(WebSecurity web) throws Exception {
-	// web.ignoring().antMatchers("/favicon.ico", "/login*", "/error", "/js/**",
-	// "/css/**", "/images/**");
-	// }
-
-	private PasswordEncoder getPasswordEncoder() {
-		return new PasswordEncoder() {
-			@Override
-			public String encode(CharSequence charSequence) {
-				return charSequence.toString();
-			}
-
-			@Override
-			public boolean matches(CharSequence charSequence, String s) {
-				return true;
-			}
-		};
+	
+	@Bean
+	public BCryptPasswordEncoder getPasswordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable().authorizeRequests()
+		http.csrf().disable()
+			.authorizeRequests()
 			.antMatchers("/favicon.ico", "/login*", "/error", "/js/**", "/css/**", "/images/**").permitAll()
 			.antMatchers("/responsabile/**").hasAuthority("RESPONSABILE")
 			.antMatchers("/direttore/**").hasAuthority("DIRETTORE")
