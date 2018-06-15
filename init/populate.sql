8886e5554b46926f8237eb04284381de2345b877
Omar Elsayed <ramorimo90@hotmail.com>
2018-06-15 12:23:35 +0200

    (init) aggiunto setting manuale della tabella sequence dopo l'aggiunta delle ennuple

--- init/populate.sql
+++ init/populate.sql
@@ -1,6 +1,8 @@
-﻿insert into centro(id, nome, capienza) values('1', 'centro-1', '100');
-insert into centro(id, nome, capienza) values('2', 'centro-2', '10');
+﻿insert into centro(id, nome, capienza) values('1','centro-1', '100');
+insert into centro(id, nome, capienza) values('2','centro-2', '10');
 
+insert into attivita(id, nome, orario_inizio, orario_fine, centro_id) 
+		values('3','Come coccolare un fenicottero','2018-08-02 12:00:00','2018-08-02 14:00:00','2');
 insert into attivita(id, nome, orario_inizio, orario_fine, centro_id) 
 		values('4','Osservazione intensa','2018-08-02 12:00:00','2018-08-02 14:00:00','1');
 insert into attivita(id, nome, orario_inizio, orario_fine, centro_id) 
@@ -19,5 +21,9 @@ insert into allievo(id, codice_fiscale, nome, cognome, email, luogo_nascita, dat
 	values('10','LGUJMP81MARIO', 'Luigi','Jumpman','super.luigi@nintendo.com','Mario Land','1981-08-03','1101010101');
 
 
-insert into attivita_allievi_prenotati(allievi_prenotati_id, attivita_prenotate_id) values('9', '4');
-insert into attivita_allievi_prenotati(allievi_prenotati_id, attivita_prenotate_id) values('9', '8');
+insert into allievo_attivita_prenotate(attivita_prenotate_id,allievi_prenotati_id) values('4','9');
+insert into allievo_attivita_prenotate(attivita_prenotate_id,allievi_prenotati_id) values('8','9');
+
+ALTER SEQUENCE public.hibernate_sequence START WITH 11;
+
+ALTER SEQUENCE public.hibernate_sequence RESTART;
