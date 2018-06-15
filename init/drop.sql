c181dc7985c90cc53bdc00fc37c776b3abf1f62d
Omar Elsayed <ramorimo90@hotmail.com>
2018-06-15 12:22:56 +0200

    (init) script sql per droppare tutte le tabelle

---
+++ init/drop.sql
@@ -0,0 +1,7 @@
+﻿drop table if exists "attivita_allievi_prenotati" cascade;
+drop table if exists "allievo_attivita_prenotate" cascade;
+drop table if exists "centro" cascade;
+drop table if exists "attivita" cascade;
+drop table if exists "allievo" cascade;
+
+DROP SEQUENCE public.hibernate_sequence;
